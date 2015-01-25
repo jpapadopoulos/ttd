1a5bab44449078d3ec7e74fa326f13ec77e820c4
Joti Papadopoulos <pan.papadopoulos80@gmail.com>
2015-01-25 21:06:53 +0200

    Functional tets now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -16,24 +17,37 @@ class NewVisitorTest(unittest.TestCase):
         # Edith has heard about a cool new online to-do app. She goes
         # check it's homepage
         self.browser.get('http://localhost:8000')
-        self.browser.implicitly_wait(3)
+        #self.browser.implicitly_wait(3)
         
         # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
         
         # She is invited to enter a to-do item right away
-        
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+                inputbox.get_attribute('placeholder'),
+                'Enter a to-do item'
+        )
+
         # She types "Buy peackock feathers" into a text box(Edith's hobby
         # is tying fly-fishing lures)
-        
+        inputbox.send_keys('Buy peacock feathers')
+
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peackock feathers" as an item in a to-do list
-        
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+                any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
         
         # There is still a text box inviting her to add another item. She
         # enters "Use peackock feathers to make a fly" (Edith is very methodical)
-        
+        self.fail('Finish the test')
         # The page updates again, and now shows items on her list
         
         # Edith wonders whether the site will remember her list. Then she sees
