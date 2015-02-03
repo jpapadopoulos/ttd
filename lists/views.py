f49f34d388ba0e22917b34dedc144a0173a8130e
Joti Papadopoulos <pan.papadopoulos80@gmail.com>
2015-02-03 18:58:02 +0200

    Redirect after a POST and accompanying test

--- lists/views.py
+++ lists/views.py
@@ -1,13 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from lists.models import Item
 
 # Create your views here.
 def home_page(request):
+    
+    if request.method == 'POST':
+        Item.objects.create(text=request.POST['item_text'])
+        return redirect('/')
 
-    item = Item()
-    item.text = request.POST.get('item_text', '')
-    item.save()
-
-    return render(request, 'home.html', {
-        'new_item_text':item.text
-    })
+    return render(request, 'home.html')
